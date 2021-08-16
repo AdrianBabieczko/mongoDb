1f58b9f80b59384bac2299002253b57a1838bc53
Adrian Babieczko <adrian.babieczko@gmail.com>
2021-08-16 10:04:02 +0200

    End of play

--- server.js
+++ server.js
@@ -14,37 +14,16 @@ MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUni
     console.log('Successfully connected to the database');
 
     const db = client.db('companyDB');
-
-    db.collection('employees').find({ department: 'IT' }, (err, data) => {
-      if (!err) {
-        data.each((error, employee) => {
-          console.log(employee);
-        })
-      }
-    });
-
-    db.collection('departments').insertOne({ name: 'Management' }, err => {
-      if (err) console.log('err');
-    });
-
-    db.collection('employees').updateOne({ department: 'IT' }, { $set: { salary: 6000 } }, err => {
-      if (err) console.log(err);
-    });
-
-    db.collection('departments').deleteOne({ name: 'Management' }, (err) => {
-      if (err) console.log(err);
-    });
-
     const app = express();
+
+    app.use(cors());
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
 
     app.use((req, res, next) => {
       req.db = db;
       next();
     });
-
-    app.use(cors());
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: false }));
 
     app.use('/api', employeesRoutes);
     app.use('/api', departmentsRoutes);
@@ -52,7 +31,7 @@ MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUni
 
     app.use((req, res) => {
       res.status(404).send({ message: 'Not found...' });
-    })
+    });
 
     app.listen('8000', () => {
       console.log('Server is running on port: 8000');
