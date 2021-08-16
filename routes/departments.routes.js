01e160fe0de2a798c572f622584d9687582f4f1a
Adrian Babieczko <adrian.babieczko@gmail.com>
2021-08-16 13:11:28 +0200

    Fully use db

--- routes/departments.routes.js
+++ routes/departments.routes.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const router = express.Router();
-// const db = require('./../db');
 const ObjectId = require('mongodb').ObjectId;
 
 router.get('/departments', (req, res) => {
-  console.log('/departments');
   req.db.collection('departments').find().toArray((err, data) => {
     if (err) res.status(500).json({ message: err })
     else res.json(data);
@@ -12,34 +10,58 @@ router.get('/departments', (req, res) => {
 });
 
 router.get('/departments/random', (req, res) => {
-  res.json(db.departments[Math.floor(Math.random() * db.length)]);
+  req.db.collection('departments').aggregate([{ $sample: { size: 1 } }]).toArray((err, data) => {
+    if (err) res.status(500).json({ message: err });
+    else res.json(data[0]);
+  });
 });
 
 router.get('/departments/:id', (req, res) => {
-  console.log('/departments/:id');
-  console.log(ObjectId(req.params.id));
-  req.db.collection('departments').findOne({ _id: ObjectId(req.params.id) }, (err, data) => {
-    if(err) res.status(500).json({ message: err });
-    else if(!data) res.status(404).json({ message: 'Not found' });
+  var myId = JSON.parse(req.params.id);
+  req.db.collection('departments').findOne({ _id: ObjectId(myId) }, (err, data) => {
+    if (err) res.status(500).json({ message: err });
+    else if (!data) res.status(404).json({ message: 'Not found' });
     else res.json(data);
   });
 });
 
 router.post('/departments', (req, res) => {
   const { name } = req.body;
-  db.departments.push({ id: 3, name })
-  res.json({ message: 'OK' });
+  req.db.collection('departments').insertOne({ name: name }, err => {
+    if (err) res.status(500).json({ message: err });
+    else res.json({ message: 'OK' });
+  });
 });
 
 router.put('/departments/:id', (req, res) => {
   const { name } = req.body;
-  db = db.departments.map(item => (item.id == req.params.id) ? { ...item, name } : item);
-  res.json({ message: 'OK' });
+  var myId = JSON.parse(req.params.id);
+
+  req.db.collection('departments').findOne({ _id: ObjectId(myId) }, (err, data) => {
+    if (err) res.status(500).json({ message: err });
+    else if (!data) res.status(404).json({ message: 'Not found' });
+    else {
+      req.db.collection('departments').updateOne({ _id: ObjectId(myId) }, { $set: { name: name } }, err => {
+        if (err) res.status(500).json({ message: err });
+        else res.json({ message: 'OK' });
+      });
+    }
+  });
 });
 
 router.delete('/departments/:id', (req, res) => {
-  db = db.departments.filter(item => item.id != req.params.id)
-  res.json({ message: 'OK' });
+  var myId = JSON.parse(req.params.id);
+
+  req.db.collection('departments').findOne({ _id: ObjectId(myId) }, (err, data) => {
+    if (err) res.status(500).json({ message: err });
+    else if (!data) res.status(404).json({ message: 'Not found' });
+    else {
+      req.db.collection('departments').deleteOne({ _id: ObjectId(myId) }, err => {
+        if (err) res.status(500).json({ message: err });
+        else res.json({ message: 'OK' });
+      });
+    }
+  });
 });
 
 module.exports = router;
